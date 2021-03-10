9659bcc66cc0fc1febcc52f2b5f9d0aedae162cd
xcornx <yilluminati1337@gmail.com>
2021-03-10 00:23:09 +0000

    aboutme

---
+++ aboutme/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Amer Firdaus</title>
+  <meta content="web security, penetration testing, cyber security, information security, Linux user" name="keywords">
+  <meta content="IT Support" name="description">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+  <link href="assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="assets/lib/hover/hover.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/css/responsive.css" rel="stylesheet">
+  <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon"/>
+  <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon"/>
+</head>
+<body>
+  <div id="header" class="home">
+    <div class="container">	
+      <div class="header-content">
+        <h1>~# <span class="typed"></span></h1>
+        <p>"Knowledge is power" - Francis Bacon 1597, ‘Meditationes Sacrae and Human Philosophy’</p>
+        <ul class="list-unstyled list-social">
+		  <li><a href="https://github.com/amerfirdaus" target="_blank"><i class="ion-social-github"></i></a></li>
+          <li><a href="https://twitter.com/@xcornx_" target="_blank"><i class="ion-social-twitter"></i></a></li>
+		  <li><a href="https://www.instagram.com/amerfirdaus__/" target="_blank"><i class="ion-social-instagram"></i></a></li>
+      </div>
+    </div>
+  </div>
+  <script src="assets/lib/jquery/jquery.min.js"></script>
+  <script src="assets/lib/jquery/jquery-migrate.min.js"></script>
+  <script src="assets/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/lib/typed/typed.js"></script>
+  <script src="assets/lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="assets/lib/magnific-popup/magnific-popup.min.js"></script>
+  <script src="assets/lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
